5db5de5c4dfa49768f11f4e50652050c219231cf
huibin <huibinliu123@163.com>
2015-04-16 21:39:35 +0800

    test

---
+++ app/src/main/java/com/demo/huibin/myandroidapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.demo.huibin.myandroidapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init() ;
+    }
+
+    private void init() {
+    }
+
+
+}
